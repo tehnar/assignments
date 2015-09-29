e2417dbd61ba889c3cb7128246b4dd2f31fc6274
tehnar <tehnar5@gmail.com>
2015-09-29 16:51:11 +0300

    Implemented classes without tests 	new file:   src/main/java/ru/spbau/mit/Collections.java 	new file:   src/main/java/ru/spbau/mit/Function1.java 	new file:   src/main/java/ru/spbau/mit/Function2.java 	new file:   src/main/java/ru/spbau/mit/Predicate.java

---
+++ src/main/java/ru/spbau/mit/Collections.java
@@ -0,0 +1,7 @@
+package ru.spbau.mit;
+
+/**
+ * Created by seva on 29.09.15.
+ */
+public class Collections {
+}
---
+++ src/main/java/ru/spbau/mit/Predicate.java
@@ -0,0 +1,7 @@
+package ru.spbau.mit;
+
+/**
+ * Created by seva on 25.09.15.
+ */
+public class Predicate {
+}
